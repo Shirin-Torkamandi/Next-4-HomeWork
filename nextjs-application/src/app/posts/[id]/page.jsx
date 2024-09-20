1270fadb918b5b526802ae041e829b5d80be7a9d
Shirin-Torkamandi <shirin.t44@gmail.com>
2024-09-20 20:38:14 +0330

    dynamic metadata for dynamic route (/[id]) based on id

--- nextjs-application/src/app/posts/[id]/page.jsx
+++ nextjs-application/src/app/posts/[id]/page.jsx
@@ -1,5 +1,13 @@
 import getData from "@/utils/getData";
 import { Container } from "@mui/material";
+
+export async function generateMetadata({ params }) {
+  return {
+    title: `Post ${params.id}`,
+    description: `This page is for the Post ${params.id}`,
+  };
+}
+
 export default async function postsId({ params }) {
   const data = await getData("https://dummyjson.com/posts");
   const postFind = data.posts.find((el) => el.id == params.id);
--- nextjs-application/src/app/recipes/[id]/page.jsx
+++ nextjs-application/src/app/recipes/[id]/page.jsx
@@ -1,5 +1,13 @@
 import getData from "@/utils/getData";
 import { Container } from "@mui/material";
+
+export async function generateMetadata({ params }) {
+  return {
+    title: `Recipe ${params.id}`,
+    description: `This page is for the Recipe ${params.id}`,
+  };
+}
+
 export default async function recipesId({ params }) {
   const data = await getData("https://dummyjson.com/recipes");
   const recipesFind = data.recipes.find((el) => el.id == params.id);
@@ -9,7 +17,10 @@ export default async function recipesId({ params }) {
         <img src={recipesFind.image} style={{ maxWidth: "40%" }} />
         <h1>{recipesFind.name}</h1>
         <p>Instructions: {recipesFind.instructions}</p>
-        <p>Time needed: {recipesFind.prepTimeMinutes + recipesFind.cookTimeMinutes} min</p>
+        <p>
+          Time needed:{" "}
+          {recipesFind.prepTimeMinutes + recipesFind.cookTimeMinutes} min
+        </p>
         <p>It is served for {recipesFind.mealType}</p>
       </Container>
     </div>
